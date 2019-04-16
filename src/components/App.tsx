bb48a632aa1b0f6cc1630f605440428a821948e4
yami-beta <yami-beta@users.noreply.github.com>
2019-04-16 17:52:12 +0900

    Visualize with svg

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,28 @@
-import React, { useEffect, useCallback, useState } from "react";
+import React, { useEffect, useCallback, useState, useMemo } from "react";
 
 const App: React.FC<{}> = () => {
-  const [deviceOrientation, setDeviceOrientation] = useState();
+  const [deviceOrientation, setDeviceOrientation] = useState({
+    absolute: false,
+    alpha: 0,
+    beta: 0,
+    gamma: 0
+  });
+
+  const cx = useMemo(() => {
+    let x = deviceOrientation.gamma;
+    return ((x + 90) / 180) * 100;
+  }, [deviceOrientation.gamma]);
+  const cy = useMemo(() => {
+    let y = deviceOrientation.beta;
+    if (y > 90) {
+      y = 90;
+    }
+    if (y < -90) {
+      y = -90;
+    }
+    return ((y + 90) / 180) * 100;
+  }, [deviceOrientation.beta]);
+
   const handleDeviceOrientation = useCallback(
     (event: DeviceOrientationEvent) => {
       setDeviceOrientation({
@@ -24,8 +45,33 @@ const App: React.FC<{}> = () => {
 
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>DeviceOrientation Sample</h1>
       <pre>{JSON.stringify(deviceOrientation, null, "  ")}</pre>
+
+      <div
+        style={{
+          position: "relative",
+          width: "100%",
+          paddingTop: "100%"
+        }}
+      >
+        <svg
+          width="500"
+          height="500"
+          viewBox="0 0 100 100"
+          xmlns="http://www.w3.org/2000/svg"
+          style={{
+            position: "absolute",
+            top: "0",
+            left: "0",
+            width: "100%",
+            height: "100%"
+          }}
+        >
+          <rect width="100" height="100" x="0" y="0" fill="#000000" />
+          <circle cx={cx} cy={cy} r="5" fill="#ffffff" />
+        </svg>
+      </div>
     </div>
   );
 };
